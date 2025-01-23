49a0da052b099968155ad7b845af25f8ad3aec3a
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-01-23 16:57:22 -0300

    Adição das bibliotecas essenciais e definição do pino de contorle do led e a quantidade dos mesmos

--- LED-Matrix-Animation.c
+++ LED-Matrix-Animation.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/clocks.h"
 
+// Biblioteca gerada pelo arquivo ws2818b.pio 
+#include "ws2818b.pio.h"
+
+// Definição da quantidade de LEDs e o pino de controle
+#define LED_COUNT 25
+#define LED_PIN 7
 
 int main () {
 
