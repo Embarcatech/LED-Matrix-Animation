39551170d6854006b833bb6a59ea73534ec19ce7
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-01-23 17:06:31 -0300

    Definição do buffer de pixel que representam a matriz de LEDs

--- LED-Matrix-Animation.c
+++ LED-Matrix-Animation.c
@@ -10,6 +10,18 @@
 #define LED_COUNT 25
 #define LED_PIN 7
 
+// Definição de pixel GRB
+struct pixel_t{
+    uint8_t G,R,B; // Três valores de 8 bits para cada cor
+};
+
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Mudança de nome para facilitar a leitura
+
+// Buffer de pixels que será enviado para o controlador
+npLED_t leds [LED_COUNT]; 
+
+
 int main () {
 
     printf("Hello, Pico!\n");
