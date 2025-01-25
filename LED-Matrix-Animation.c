5471aa949f2759b8ac00096bd27a549f8c96ebac
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-01-25 00:23:52 -0300

    Ajuste na inicialização da PIO e adição da função matriz_rgb().

--- LED-Matrix-Animation.c
+++ LED-Matrix-Animation.c
@@ -30,6 +30,16 @@ typedef pixel_t npLED_t; // Mudança de nome para facilitar a leitura
 // Buffer de pixels que será enviado para o controlador
 npLED_t leds [LED_COUNT]; 
 
+
+// Função para converter cores em formato RGB para o protocolo da matriz de LEDs
+uint32_t matriz_rgb(uint8_t b, uint8_t r, uint8_t g) {
+    unsigned char R, G, B;
+    R = (unsigned char)r;
+    G = (unsigned char)g;
+    B = (unsigned char)b;
+    return (G << 24) | (R << 16) | (B << 8);
+}
+
 // Variváveis para o controle do PIO
 PIO np_pio;
 uint sm;
@@ -48,7 +58,7 @@ void npInit(uint pin){
     }
 
     // Inicializa o programa na máquina PIO
-    ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+    ws2818b_program_init(np_pio, sm, offset, pin);
 
     // Limpa o buffer de LEDs
     for(uint i = 0; i < LED_COUNT; i++){
@@ -75,9 +85,8 @@ void npClear(){
 void npWrite(){
     // Escreve cada dado de 8 bits dos pixels no buffer da máquina PIO
     for (uint i = 0; i < LED_COUNT; i++){
-        pio_sm_put_blocking(np_pio, sm, leds[i].G);
-        pio_sm_put_blocking(np_pio, sm, leds[i].R);
-        pio_sm_put_blocking(np_pio, sm, leds[i].B);
+        uint32_t valor_led = matriz_rgb(leds[i].B, leds[i].R, leds[i].G);
+        pio_sm_put_blocking(np_pio, sm, valor_led);
     }
     sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
 }
@@ -104,11 +113,11 @@ int main () {
     npClear();
     
     //exemplo de uso da função npSetLED para acender um LED de cada vez
-    animacaoBasica();
-    //npSetLED(0, 128, 0, 0); // A título de teste, atribui a cor vermelha ao primeiro LED com 50% de intensidade
+    //animacaoBasica();
+    npSetLED(0, 128, 0, 0); // A título de teste, atribui a cor vermelha ao primeiro LED com 50% de intensidade
 
     // Escreve o buffer de LEDs no controlador
-//    npWrite();
+   npWrite();
 
     while(true){
         sleep_ms(1000);
