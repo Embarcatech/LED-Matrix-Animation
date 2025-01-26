dc838b94158e67d121a86104d1594469429de487
alinnecaastro <alinnecastro133@gmail.com>
2025-01-25 21:32:10 -0300

    adição de mais uma animação

--- LED-Matrix-Animation.c
+++ LED-Matrix-Animation.c
@@ -71,7 +71,73 @@ void animacaoTecla0(){
     sleep_ms(200);
 }
 
+//Codigo Alinne 
 
+// INICIO DA 1 ANIMAÇÃO
+void animacaoQuadradoPulsante(){
+    //Define as posições dos LEDs que formarão o quadrado
+    const uint led_sequence[] = {0,9,10,19,20,21,22,23,24,15,14,5,4,3,2,1,0,12};
+    const uint led_count = sizeof(led_sequence)/ sizeof(led_sequence[0]);
+
+    for(int i = 0; i<led_count; i++){
+        if(led_sequence[i]==12){
+            npSetLED(led_sequence[i],255,0,255);
+        }else{
+        npSetLED(led_sequence[i],255,0,0);
+        }
+    }
+        npWrite();
+        sleep_ms(10);
+    
+
+    for (uint i =0; i<led_count; i++){
+        npSetLED(led_sequence[i],0,0,0);
+        npWrite();
+        sleep_ms(200);
+    }
+}
+// FIM DA 1 ANIMAÇÃO
+
+
+//INICIO DA SEGUNDA ANIMAÇÃO
+
+void animacaoOnda(){
+   //Define as posiçoes dos LEDs que formarão a sequencia
+   const uint led_sequence[] = {0,9,10,19,20,21,18,11,8,1,2,7,12,17,22,23,16,13,6,3,4,5,14,15,24};
+    const uint led_count = sizeof(led_sequence) / sizeof(led_sequence[0]);
+
+    //Define a velocidade do cometa e do rastro
+    const uint tempo_rastro = 100;
+    const uint tempo_cometa = 100;
+    
+    //Define as cores
+    const uint8_t cometa_R = 255;
+    const uint8_t cometa_G = 0;
+    const uint8_t cometa_B = 0;
+    const uint8_t rastro_R = 0;
+    const uint8_t rastro_G = 0;
+    const uint8_t rastro_B = 255;
+
+    npClear();//Limpa os LEDs antes de iniciar a animação
+
+    //Faz o movimento de onda
+    for(uint i = 0; i<led_count;i++){
+        npClear();
+
+        //Acende o rastro até a posição atual
+        for(uint j=0; j<=i; j++){
+            npSetLED(led_sequence[j],rastro_R,rastro_G,rastro_B);//rastro azul
+            }
+
+            //Acende o cometa na posição atual
+            npSetLED(led_sequence[i],cometa_R,cometa_G,cometa_B);//Cometa vermelho
+
+            npWrite(); //Atualiza os LEDs
+            sleep_ms(tempo_rastro);
+    }
+            npClear();
+            npWrite();
+}
 int main () {
     stdio_init_all();
 
@@ -81,8 +147,11 @@ int main () {
     npClear();
     
     //exemplo de uso da função npSetLED para acender um LED de cada vez
-    animacaoBasica();
+    //animacaoBasica();
     //npSetLED(0, 128, 0, 0); // A título de teste, atribui a cor vermelha ao primeiro LED com 50% de intensidade
+    //anim
+    animacaoQuadradoPulsante();
+    animacaoOnda();
 
     // Escreve o buffer de LEDs no controlador
    //npWrite();
