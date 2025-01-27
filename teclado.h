d92a839fb98b9f28863c23ee7bc432c19f5d9e08
HUGO S. DIAS <39714844+hsantosdias@users.noreply.github.com>
2025-01-27 16:19:31 -0300

    Mudança, tentativa de resolver problema da coluna 1 da matriz[4][4], tudo nulo

--- teclado.h
+++ teclado.h
@@ -32,7 +32,7 @@ void iniciar_teclado() {
     for (int i = 0; i < 4; i++) {
         gpio_init(LINHAS[i]);
         gpio_set_dir(LINHAS[i], GPIO_OUT);
-        gpio_put(LINHAS[i], 0);
+        gpio_put(LINHAS[i], 1); // Nível alto para as linhas inativas
     }
 
     // Definição das colunas como entradas com resistores de pull-up
@@ -46,27 +46,31 @@ void iniciar_teclado() {
 // Varredura do teclado e retorno da tecla pressionada
 char leitura_teclado() {
     for (int row = 0; row < 4; row++) {
-        // Coloca a linha atual em nível baixo
+        // Coloca todas as linhas em nível alto antes de ativar uma única linha
+        for (int i = 0; i < 4; i++) {
+            gpio_put(LINHAS[i], 1);
+         //   printf("GPIO Linha: %d, GPIO Coluna: %d\n", LINHAS[row], COLUNAS[col]);
+         //   printf("Tecla pressionada: %c\n", teclas[row][col]);
+        }
+
+        // Ativa a linha atual (nível baixo)
         gpio_put(LINHAS[row], 0);
 
         for (int col = 0; col < 4; col++) {
             // Verifica se a tecla foi pressionada
             if (!gpio_get(COLUNAS[col])) {
-                printf("GPIO Linha: %d, GPIO Coluna: %d\n", LINHAS[row], COLUNAS[col]);
-                printf("Tecla pressionada: %c\n", teclas[row][col]);
-
-                // Espera um tempo para estabilização da tecla pressionada
+                // Espera para estabilização da tecla pressionada
                 sleep_ms(150);
 
-                // Aguarda a tecla ser liberada antes de continuar
-                while (!gpio_get(COLUNAS[col]));
-
-                gpio_put(LINHAS[row], 1); // Restaura a linha atual
-                return teclas[row][col];
+                // Certifica-se de que a tecla ainda está pressionada
+                if (!gpio_get(COLUNAS[col])) {
+                    gpio_put(LINHAS[row], 1); // Reseta a linha atual
+                    return teclas[row][col];
+                }
             }
         }
 
-        // Restaura a linha atual para nível alto
+        // Desativa a linha atual (volta ao nível alto)
         gpio_put(LINHAS[row], 1);
     }
 
