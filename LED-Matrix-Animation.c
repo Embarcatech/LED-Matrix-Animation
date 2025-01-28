58a13ea255290f323a8c76db12e0cc46aafdb2ac
HUGO S. DIAS <39714844+hsantosdias@users.noreply.github.com>
2025-01-28 14:40:47 -0300

    Adição de codigo animação Jackson

--- LED-Matrix-Animation.c
+++ LED-Matrix-Animation.c
@@ -19,6 +19,9 @@
 //Animações Gabriel
 #include "libs/animacao_gabriel.c"
 
+//Jackson S. dos Santos
+#include "libs/animacao_jackson.c"
+
 // Definições gerais do projeto
 #include "libs/definicoes.h"
 
@@ -62,59 +65,6 @@ void tocarMusicaCurta() {
     pwm_set_enabled(slice_num, false);
 
 }
-
-// INÍCIO DA ANIMAÇÃO 0 - Jackson S. dos Santos
-
-// Função para configurar um LED e esperar
-void configurarLED(uint posicao, uint8_t red, uint8_t blue, uint8_t green, uint tempoDeEspera){
-    npSetLED(posicao, red, blue, green);
-    npWrite();
-    sleep_ms(tempoDeEspera);
-}
-
-// Função para configurar vários LEDs e esperar
-void configurarLEDs(uint *posicoes, uint tamanho, uint8_t red, uint8_t green, uint8_t blue, uint tempoDeEspera){
-    npSetManyLEDs(posicoes, tamanho, red, green, blue);
-    npWrite();
-    sleep_ms(tempoDeEspera);
-}
-
-//ok implementado
-void animacaoTecla0(){
-    // Define as posições dos LEDs que formarão o quadrado
-    uint posicaoCentral = 12; 
-    uint posicoesInterno[8] = {6, 7, 8, 11, 13, 16, 17, 18};
-    uint posicoesBorda[16] = {0, 1, 2, 3, 4, 9, 14, 19, 24, 23, 22, 21, 20, 15, 10, 5};
-    uint tempoDeEspera = 200; 
-    
-    // Acende o LED 12 em vermelho e apaga em seguida
-    configurarLED(posicaoCentral, 255, 0, 0, tempoDeEspera);
-    configurarLED(posicaoCentral, 0, 0, 0, tempoDeEspera);
-
-    // Acende LEDs internos em azul e apaga em seguida
-    configurarLEDs(posicoesInterno, 8, 0, 0, 255, tempoDeEspera);
-    npClear();
-    sleep_ms(tempoDeEspera);
-
-    // Pisca a borda em vermelho e azul
-    configurarLEDs(posicoesBorda, 16, 255, 0, 0, tempoDeEspera);
-    npClear();
-    sleep_ms(tempoDeEspera);
-    configurarLEDs(posicoesBorda, 16, 0, 0, 255, tempoDeEspera);
-    npClear();
-    sleep_ms(tempoDeEspera);
-
-    // Acende LEDs internos em vermelho e apaga
-    configurarLEDs(posicoesInterno, 8, 255, 0, 0, tempoDeEspera);
-    npClear();
-    sleep_ms(tempoDeEspera);
-
-    // Finaliza com LED 12 em azul
-    configurarLED(posicaoCentral, 0, 0, 255, tempoDeEspera);
-
-}
-
-// FIM DA ANIMAÇÃO 0
 
 //Codigo Alinne 
 
